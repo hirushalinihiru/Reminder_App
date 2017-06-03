cbf2379a479b56564679d06bedc9cf40002ec721
hirushalinihiru <hirushalini@hotmail.com>
2017-06-03 22:45:22 +0530

    HIRUSHALINI EDIT

--- app/src/main/java/lk/sliit/mad/reminder_app/MainActivity.java
+++ app/src/main/java/lk/sliit/mad/reminder_app/MainActivity.java
@@ -1,7 +1,6 @@
 package lk.sliit.mad.reminder_app;
 
 import android.app.AlarmManager;
-import android.app.DatePickerDialog;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -10,15 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText date;
-    DatePickerDialog datePickerDialog;
-    Calendar cal;
 
     Button btnOneTime,btnRepeat;
 
@@ -26,38 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // initiate the date picker and a button
-        date = (EditText) findViewById(R.id.date);
-        // perform click event on edit text
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // calender class's instance and get current date , month and year from calender
-                final Calendar c = Calendar.getInstance();
-                cal=c;
-                int mYear = c.get(Calendar.YEAR); // current year
-                int mMonth = c.get(Calendar.MONTH); // current month
-                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-                // date picker dialog
-                datePickerDialog = new DatePickerDialog(MainActivity.this,
-                        new DatePickerDialog.OnDateSetListener() {
-
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
-                                // set day of month , month and year value in the edit text
-                                date.setText(dayOfMonth + "/"
-                                        + (monthOfYear + 1) + "/" + year);
-
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
-            }
-        });
-
-
-
 
         btnOneTime = (Button) findViewById(R.id.btnOneTime);
         btnRepeat = (Button) findViewById(R.id.btnRepeating);
@@ -81,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void startAlarm(boolean isRepeat) {
         AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent myIntent = new Intent(MainActivity.this,AlarmNotificationReceiver.class);
+        Intent myIntent = new Intent(MainActivity.this,AlarmNotificationReciever.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this,0,myIntent,0);
 
         if(!isRepeat)
         {
-            manager.setExact(AlarmManager.RTC_WAKEUP,cal.getTimeInMillis(),pendingIntent);
+            manager.set(AlarmManager.RTC_WAKEUP,SystemClock.elapsedRealtime()+3000,pendingIntent);
         }
         else
         {
